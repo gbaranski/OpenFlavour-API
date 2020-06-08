17c65cfac1114cc21e37a903e74440651dfc63e5
gbaranski <gbaranski19@gmail.com>
2020-06-08 14:31:51 +0200

    split into two paths

--- src/routes/api-routes.ts
+++ src/routes/api-routes.ts
@@ -3,14 +3,30 @@ import { MongoClient } from 'mongodb';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/byManufacturer', (req, res) => {
   MongoClient.connect('mongodb://localhost:27017', async (err, database) => {
     if (database) {
       const flavor = await database
         .db('flavors')
         .collection('test')
         .find({
-          manufacturer: 'TPA',
+          manufacturer: { $regex: `.*${req.query.key?.toString().toUpperCase()}.*` },
+        })
+        .toArray();
+      res.json(flavor);
+      database.close();
+    }
+  });
+});
+
+router.get('/byName', (req, res) => {
+  MongoClient.connect('mongodb://localhost:27017', async (err, database) => {
+    if (database) {
+      const flavor = await database
+        .db('flavors')
+        .collection('test')
+        .find({
+          name: { $regex: `.*${req.query.key}.*` },
         })
         .toArray();
       res.json(flavor);
